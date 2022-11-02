92881406a183d3fdb0b3ff9e6680f57c1d3c0c60
mustang81 <39166821+mustang81@users.noreply.github.com>
2022-11-02 20:04:07 +0500

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://desaleemi.com/V3</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://desaleemi.com/V3/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://desaleemi.com/V3/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://desaleemi.com/V3/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://desaleemi.com/V3/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://desaleemi.com/V3/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
